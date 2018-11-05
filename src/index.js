abb43dc66ef8d085e97c2f859f741865c537a9bf
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2018-11-05 19:42:13 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const seprator = () => {
-    console.log("========>");
+    console.log("----------------------------");
 };
 
 seprator();
@@ -15,6 +15,41 @@ parseInt(
 ) * Math.sign(inte);
 
 console.log(rev(1234));
-
+seprator();
 
 //string reverse
+const revStr = string => 
+    string
+    .split("")
+    .reverse()
+    .join("");
+
+var testStr = "hello";
+console.log(revStr(testStr));
+seprator();
+
+const __rev = string =>{
+    let result = "";
+    for (let ch of string){
+        result = ch + result;
+    }
+        return result;
+    
+};
+
+console.log(__rev(testStr));
+seprator();
+
+//pali
+const __pali = string => {
+    var slen = string.length;
+    for(var i=0; i < slen / 2; i++){
+        if(string[i] !== string[slen - i - 1])
+        return false;
+    }
+    return true;
+}
+
+var pstr = "00000";
+console.log(__pali(pstr));
+seprator();
