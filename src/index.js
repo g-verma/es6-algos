a2171540688ccaee3b5d0db2a1295561770251f9
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2018-11-05 17:39:34 +0530

    Branch updated

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const seprator = () => {
+    console.log("========>");
+};
+
+seprator();
+
+//int reverse
+const rev = inte => 
+parseInt(
+    inte
+    .toString()
+    .split("")
+    .reverse()
+    .join("")
+) * Math.sign(inte);
+
+console.log(rev(1234));
+
+
+//string reverse
