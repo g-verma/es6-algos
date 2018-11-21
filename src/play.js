c5bc72dcac6f35de1a2e44876379d1129a1f61d0
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2018-11-21 16:16:44 +0530

    updated

--- src/play.js
+++ src/play.js
@@ -1,23 +1,9 @@
-// sorted bbs
-const __bsrt = arr =>{
-    var done = true;
+//subset sum
 
-    for(var i=0; i< arr.length; i++){
-      for(var j=0; j<arr.length; j++){       
-            if(arr[j] > arr[j+1]){
-                
-                var temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp;
-                done = false;
-            }
-        }
-        if(done) return;
-        
-    }
+const __subSetSum = (arr, el) =>{
 
-    return arr;    
 }
 
-var inparr = [188, 5, 8, 1, 2, 15, 3];
-console.log("sorted Bs: ", __bsrt(inparr));
+var inarr = [3, 34, 4, 12, 5, 2];
+var sum = 9;
+console.log(__subSetSum())
