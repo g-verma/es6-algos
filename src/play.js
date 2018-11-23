01d4ce77347efd30bf87ecf9cc3dae7f7c00a3bc
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2018-11-23 15:56:41 +0530

    updated

--- src/play.js
+++ src/play.js
@@ -1,161 +1,138 @@
 
-
-
+// smallest subarray with sum greater than given sum , time complexity O(n), can also calulate negative numbers
 const __smallWi = (arr, n, x) =>{
     var currSum = 0,
      minlen = n+1,
-     start =  0, 
+     start = 0, 
      end = 0;
 
-     while(){
-        while(){
-            if(){}
+     while(end < n){
+        while (currSum <= x && end < n) {
+            if (currSum <= 0 && x > 0) 
+            { 
+                start = end; 
+                currSum = 0; 
+            } 
+  
+            currSum += arr[end++]; 
         }
 
-        while(){
-            if(){}
+        while(currSum > x && start < n) {
+            if (end - start < minlen) 
+                minlen = end - start; 
+            currSum -= arr[start++]; 
         }
      }
     
+     return minlen; 
 
 }
 
+var arri = [- 8, 1, 4, 2, -6];
+var x =6;
+console.log('smallest subarray having sum greater than sum: ', __smallWi(arri,arri.length, x));
 
-// Returns length of smallest subarray with sum greater than x. 
-int smallestSubWithSum(int arr[], int n, int x) 
-{ 
-    // Initialize current sum and minimum length 
-    int curr_sum = 0, min_len = n+1; 
-  
-    // Initialize starting and ending indexes 
-    int start = 0, end = 0; 
-    while (end < n) 
-    { 
-        // Keep adding array elements while current sum 
-        // is smaller than x 
-        while (curr_sum <= x && end < n) 
-        { 
-            // Ignore subarrays with negative sum if 
-            // x is positive. 
-            if (curr_sum <= 0 && x > 0) 
-            { 
-                start = end; 
-                curr_sum = 0; 
-            } 
-  
-            curr_sum += arr[end++]; 
-        } 
-  
-        // If current sum becomes greater than x. 
-        while (curr_sum > x && start < n) 
-        { 
-            // Update minimum length if needed 
-            if (end - start < min_len) 
-                min_len = end - start; 
-  
-            // remove starting elements 
-            curr_sum -= arr[start++]; 
-        } 
-    } 
-    return min_len; 
-} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const problem = {
+//     start: {A: 5, B: 2},
+//     A: {C: 4, D: 2},
+//     B: {A: 8, D: 7},
+//     C: {D: 6, finish: 3},
+//     D: {finish: 1},
+//     finish: {}
+//   };
   
 
-    int arr1[] = {- 8, 1, 4, 2, -6}; 
-    int x = 6; 
-    int n1 = sizeof(arr1)/sizeof(arr1[0]); 
-    int res1 = smallestSubWithSum(arr1, n1, x); 
-    (res1 == n1+1)? cout << "Not possible\n" : 
-                    cout << res1 << endl; 
+// const lowestCostNode = (costs, processed) => {
 
-
-
-
-
-
-
-
-const problem = {
-    start: {A: 5, B: 2},
-    A: {C: 4, D: 2},
-    B: {A: 8, D: 7},
-    C: {D: 6, finish: 3},
-    D: {finish: 1},
-    finish: {}
-  };
-  
-
-const lowestCostNode = (costs, processed) => {
-
-    return Object.keys(costs).reduce((lowest, node) => {
+//     return Object.keys(costs).reduce((lowest, node) => {
       
-      if (lowest === null || costs[node] < costs[lowest]) {
+//       if (lowest === null || costs[node] < costs[lowest]) {
        
-        if (!processed.includes(node)) {
-          lowest = node;
-        }
+//         if (!processed.includes(node)) {
+//           lowest = node;
+//         }
      
-      }
+//       }
 
-      return lowest;
-    }, null);
+//       return lowest;
+//     }, null);
 
-};
-
-
+// };
 
 
-  const dijkstra = (graph) => {
+
+
+//   const dijkstra = (graph) => {
   
-    // track lowest cost to reach each node
-    const costs = Object.assign({finish: Infinity}, graph.start);
+//     // track lowest cost to reach each node
+//     const costs = Object.assign({finish: Infinity}, graph.start);
   
-    // track paths
-    const parents = {finish: null};
-    for (let child in graph.start) {
-      parents[child] = 'start';
-    }
+//     // track paths
+//     const parents = {finish: null};
+//     for (let child in graph.start) {
+//       parents[child] = 'start';
+//     }
   
-    // track nodes that have already been processed
-    const processed = [];
+//     // track nodes that have already been processed
+//     const processed = [];
   
-    let node = lowestCostNode(costs, processed);
+//     let node = lowestCostNode(costs, processed);
   
-    while (node) {
-      let cost = costs[node];
-      let children = graph[node];
-      for (let n in children) {
-        let newCost = cost + children[n];
-        if (!costs[n]) {
-          costs[n] = newCost;
-          parents[n] = node;
-        }
-        if (costs[n] > newCost) {
-          costs[n] = newCost;
-          parents[n] = node;
-        }
-      }
-      processed.push(node);
-      node = lowestCostNode(costs, processed);
-    }
+//     while (node) {
+//       let cost = costs[node];
+//       let children = graph[node];
+//       for (let n in children) {
+//         let newCost = cost + children[n];
+//         if (!costs[n]) {
+//           costs[n] = newCost;
+//           parents[n] = node;
+//         }
+//         if (costs[n] > newCost) {
+//           costs[n] = newCost;
+//           parents[n] = node;
+//         }
+//       }
+//       processed.push(node);
+//       node = lowestCostNode(costs, processed);
+//     }
   
-    let optimalPath = ['finish'];
-    let parent = parents.finish;
-    while (parent) {
-      optimalPath.push(parent);
-      parent = parents[parent];
-    }
-    optimalPath.reverse();
+//     let optimalPath = ['finish'];
+//     let parent = parents.finish;
+//     while (parent) {
+//       optimalPath.push(parent);
+//       parent = parents[parent];
+//     }
+//     optimalPath.reverse();
   
-    const results = {
-      distance: costs.finish,
-      path: optimalPath
-    };
+//     const results = {
+//       distance: costs.finish,
+//       path: optimalPath
+//     };
   
-    return results;
-  };
+//     return results;
+//   };
   
-  console.log(dijkstra(problem));
+//   console.log(dijkstra(problem));
 
 
 
