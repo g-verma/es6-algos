90954c6a3c7ff3ad25b66bc1d706e90b60607b03
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2018-11-23 19:57:26 +0530

    updated

--- src/play.js
+++ src/play.js
@@ -1,48 +1,27 @@
-
-//Count the subarrays having product less than k
-const __sumLessK = (a, k)  =>{
-    p = 1, 
-    res = 0,
-    start = 0,
-    end =0;
-
-    for (start ; end < a.length ; end++) { 
+function quickSort(array){
+    let pivot = 0
+    let left = []
+    let right = []
   
-        p *= a[end]; 
-          
-        while (start < end && p >= k)  
-            p /= a[start++];         
-         
-        if (p < k) { 
-           var len = end-start+1; 
-            res += len; 
-        } 
-    } 
+    if (array.length <=1){
+      return array
+    } else {
+      pivot = array.pop(0)
+      for(x in array){
+        if (array[x] < pivot){
+          left.push(array[x])
+        } else if (array[x] >= pivot){
+          right.push(array[x])
+        }
+      
+      }
+    left = quickSort(left)
+    right = quickSort(right)  
+    }
+    
+    sorted = left.concat([pivot]).concat(right)  
+    return sorted;
+  }
   
-    return res; 
-} 
-var arrin = [1, 2, 3, 4];   //output is 7
-console.log("product of sum less than k is:", __sumLessK(arrin, 10));
-
-
-// const __maxStrcase = num => {
-//     var test;
-
-//     while(test--){
-//         var n, ans = 0, curr =1;
-//         n = num;
-//         while(curr <= n){
-//             n -=curr;
-//             curr++;
-//             ans++;
-//         }
-
-//         console.log("hshsh", ans)
-//     }
-
-//     return 0;
-// }
-
-// __maxStrcase(5);
-
-
+  myArr = [1,3,2,34,2,6,4,90,9,7,5]
+  console.log("quicksort array: ", quickSort(myArr));
