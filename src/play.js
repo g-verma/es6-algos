215dbfa868b3a0923a90c625c19ece292a6ecc79
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2018-11-23 15:21:21 +0530

    updated

--- src/play.js
+++ src/play.js
@@ -1,34 +1,72 @@
-  
-//staircase combinations using dynamic programming 
-// const __stc = n =>{   //complexity is O(n)
-//     var res = [n+1];
-//     res[0] = 1;
-//     res[1] = 1;
-//     res[2] = 2;
-
-//     for(var i = 3; i <= n; i++){
-//         res[i] = res[i-1] + res[i-2] + res[i-3];
-//     }
-//     return res[n];
-// }
-// console.log(__stc(3));  // input 3, output 4 :: input 4, ouput 7
 
 
 
-const __sst = n =>{
-var res = [];
-res[0]= 1;
-res[1]= 1;
-res[2] = 2;
+const __smallWi = (arr, n, x) =>{
+    var currSum = 0,
+     minlen = n+1,
+     start =  0, 
+     end = 0;
 
-for(var i = 3; i <= n ; i++){
-    res[i] = res[i-1] + res[i-2] + res[i-3];
-}
-    return res[n];
+     while(){
+        while(){
+            if(){}
+        }
+
+        while(){
+            if(){}
+        }
+     }
+    
+
 }
 
 
-console.log('ways is: ', __sst(4));
+// Returns length of smallest subarray with sum greater than x. 
+int smallestSubWithSum(int arr[], int n, int x) 
+{ 
+    // Initialize current sum and minimum length 
+    int curr_sum = 0, min_len = n+1; 
+  
+    // Initialize starting and ending indexes 
+    int start = 0, end = 0; 
+    while (end < n) 
+    { 
+        // Keep adding array elements while current sum 
+        // is smaller than x 
+        while (curr_sum <= x && end < n) 
+        { 
+            // Ignore subarrays with negative sum if 
+            // x is positive. 
+            if (curr_sum <= 0 && x > 0) 
+            { 
+                start = end; 
+                curr_sum = 0; 
+            } 
+  
+            curr_sum += arr[end++]; 
+        } 
+  
+        // If current sum becomes greater than x. 
+        while (curr_sum > x && start < n) 
+        { 
+            // Update minimum length if needed 
+            if (end - start < min_len) 
+                min_len = end - start; 
+  
+            // remove starting elements 
+            curr_sum -= arr[start++]; 
+        } 
+    } 
+    return min_len; 
+} 
+  
+
+    int arr1[] = {- 8, 1, 4, 2, -6}; 
+    int x = 6; 
+    int n1 = sizeof(arr1)/sizeof(arr1[0]); 
+    int res1 = smallestSubWithSum(arr1, n1, x); 
+    (res1 == n1+1)? cout << "Not possible\n" : 
+                    cout << res1 << endl; 
 
 
 
@@ -37,78 +75,87 @@ console.log('ways is: ', __sst(4));
 
 
 
-// const problem = {
-//     start: {A: 5, B: 2},
-//     A: {C: 4, D: 2},
-//     B: {A: 8, D: 7},
-//     C: {D: 6, finish: 3},
-//     D: {finish: 1},
-//     finish: {}
-//   };
+const problem = {
+    start: {A: 5, B: 2},
+    A: {C: 4, D: 2},
+    B: {A: 8, D: 7},
+    C: {D: 6, finish: 3},
+    D: {finish: 1},
+    finish: {}
+  };
   
-//   const lowestCostNode = (costs, processed) => {
-//     return Object.keys(costs).reduce((lowest, node) => {
-//       if (lowest === null || costs[node] < costs[lowest]) {
-//         if (!processed.includes(node)) {
-//           lowest = node;
-//         }
-//       }
-//       return lowest;
-//     }, null);
-//   };
+
+const lowestCostNode = (costs, processed) => {
+
+    return Object.keys(costs).reduce((lowest, node) => {
+      
+      if (lowest === null || costs[node] < costs[lowest]) {
+       
+        if (!processed.includes(node)) {
+          lowest = node;
+        }
+     
+      }
+
+      return lowest;
+    }, null);
+
+};
+
+
+
+
+  const dijkstra = (graph) => {
   
-//   // function that returns the minimum cost and path to reach Finish
-//   const dijkstra = (graph) => {
+    // track lowest cost to reach each node
+    const costs = Object.assign({finish: Infinity}, graph.start);
   
-//     // track lowest cost to reach each node
-//     const costs = Object.assign({finish: Infinity}, graph.start);
+    // track paths
+    const parents = {finish: null};
+    for (let child in graph.start) {
+      parents[child] = 'start';
+    }
   
-//     // track paths
-//     const parents = {finish: null};
-//     for (let child in graph.start) {
-//       parents[child] = 'start';
-//     }
+    // track nodes that have already been processed
+    const processed = [];
   
-//     // track nodes that have already been processed
-//     const processed = [];
+    let node = lowestCostNode(costs, processed);
   
-//     let node = lowestCostNode(costs, processed);
+    while (node) {
+      let cost = costs[node];
+      let children = graph[node];
+      for (let n in children) {
+        let newCost = cost + children[n];
+        if (!costs[n]) {
+          costs[n] = newCost;
+          parents[n] = node;
+        }
+        if (costs[n] > newCost) {
+          costs[n] = newCost;
+          parents[n] = node;
+        }
+      }
+      processed.push(node);
+      node = lowestCostNode(costs, processed);
+    }
   
-//     while (node) {
-//       let cost = costs[node];
-//       let children = graph[node];
-//       for (let n in children) {
-//         let newCost = cost + children[n];
-//         if (!costs[n]) {
-//           costs[n] = newCost;
-//           parents[n] = node;
-//         }
-//         if (costs[n] > newCost) {
-//           costs[n] = newCost;
-//           parents[n] = node;
-//         }
-//       }
-//       processed.push(node);
-//       node = lowestCostNode(costs, processed);
-//     }
+    let optimalPath = ['finish'];
+    let parent = parents.finish;
+    while (parent) {
+      optimalPath.push(parent);
+      parent = parents[parent];
+    }
+    optimalPath.reverse();
   
-//     let optimalPath = ['finish'];
-//     let parent = parents.finish;
-//     while (parent) {
-//       optimalPath.push(parent);
-//       parent = parents[parent];
-//     }
-//     optimalPath.reverse();
+    const results = {
+      distance: costs.finish,
+      path: optimalPath
+    };
   
-//     const results = {
-//       distance: costs.finish,
-//       path: optimalPath
-//     };
+    return results;
+  };
   
-//     return results;
-//   };
-  
-//   console.log(dijkstra(problem));
+  console.log(dijkstra(problem));
 
 
 
