acc3681f0f4cfe5f858bdcef442d36201445a765
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2018-11-29 15:29:59 +0530

    updated

--- src/play.js
+++ src/play.js
@@ -1,9 +1,42 @@
-var pp = new Promise((resolve,reject)=>{
-    setTimeout(() =>{
-        resolve('-->');
-    }, 300);
-});
+//Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, … shows the first 11 ugly numbers. By convention, 1 is included.
+//Given a number n, the task is to find n’th Ugly number.
 
-pp.then(value=>{return console.log(value + ' hmm')});
 
-console.log(pp);
+const __getNthUglyNo = n =>{ 
+    var ugly = [n]; 
+    var i2 = 0, i3 = 0, i5 = 0; 
+    var next_multiple_of_2 = 2; 
+    var next_multiple_of_3 = 3; 
+    var next_multiple_of_5 = 5; 
+    var next_ugly_no = 1; 
+  
+    ugly[0] = 1; 
+
+    for (let i=1; i<n; i++) 
+    { 
+       next_ugly_no = Math.min(next_multiple_of_2, 
+                           Math.min(next_multiple_of_3, 
+                               next_multiple_of_5)); 
+       ugly[i] = next_ugly_no; 
+
+       if (next_ugly_no == next_multiple_of_2) 
+       { 
+           i2 = i2+1; 
+           next_multiple_of_2 = ugly[i2]*2; 
+       } 
+       if (next_ugly_no == next_multiple_of_3) 
+       { 
+           i3 = i3+1; 
+           next_multiple_of_3 = ugly[i3]*3; 
+       } 
+       if (next_ugly_no == next_multiple_of_5) 
+       { 
+           i5 = i5+1; 
+           next_multiple_of_5 = ugly[i5]*5; 
+       } 
+    }
+
+    return next_ugly_no; 
+} 
+  
+console.log("ulgy no: ", __getNthUglyNo(150));
