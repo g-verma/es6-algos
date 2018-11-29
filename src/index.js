4c122ff07190b3f15a4afbb68ef70e4c778c2cbf
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2018-11-29 17:37:19 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1531,3 +1531,8 @@ console.log('typeof hello:', typeof 'hello');
 console.log('typeof true:', typeof true); 
 console.log('typeof [1,2,3]:', typeof [1,2,3]);
 console.log('typeof {name:John}:', typeof {name:'John'});
+
+//conditional
+if (num === 1){ num--; } else { num++;
+}
+(num === 1) ? num-- : num++;
--- src/play.js
+++ src/play.js
@@ -1,5 +1,2 @@
-console.log('typeof num:', typeof num); 
-console.log('typeof hello:', typeof 'hello'); 
-console.log('typeof true:', typeof true); 
-console.log('typeof [1,2,3]:', typeof [1,2,3]);
-console.log('typeof {name:John}:', typeof {name:'John'});
+
+    
