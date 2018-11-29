3169c7da7606906ded7945ce529816c885a9a10f
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2018-11-29 16:15:12 +0530

    updated

--- src/play.js
+++ src/play.js
@@ -1,31 +1,15 @@
-const __ugno = n =>{
-    var ugly = [n];  //will store ugly numbers here
-    var i2 = i3 = i5 = 0;
-    var nm2 = 2,
-        nm3 = 3,
-        nm5 = 5;
-    var nxtUgly = 1;
-    ugly[0] = 1;  //initialised ulgy array with 1
+//ugly number with while
+
+const __isUgly = num => {
+    var arr = [2, 3, 5];
     
-    for(let i=1; i<n; i++){
-        nxtUgly=Math.min(nm2, Math.min(nm3,nm5));
-        ugly[i] = nxtUgly;
-
-        if(nxtUgly == nm2){
-            i2 += 1;
-            nm2 = ugly[i2]*2;
+    arr.forEach( prime => {
+        while (num && num % prime === 0) {
+            num /= prime;    // num = num/prime
         }
-        if(nxtUgly == nm3){
-            i3 += 1;
-            nm3 = ugly[i3]*3;
-        }
-        if(nxtUgly == nm5){
-            i5 += 1;
-            nm5 = ugly[i5]*5;
-        }
-    }
-
-    return nxtUgly;
+    });
+    
+    return num === 1;
 }
 
-console.log(" Ugly ---> ",__ugno(150));
+console.log("ugly is", __isUgly(150))
