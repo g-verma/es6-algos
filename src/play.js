250b9df6a525bea8fe50c49e0e46923f64357244
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2019-04-19 16:35:30 +0530

    updated

--- src/play.js
+++ src/play.js
@@ -1,26 +1,54 @@
-//   // Solution 2  Using dynamic programming     complexity is O(n)
-//   const __waysToClimb = steps =>{   
-//     const result = [1,1,2];  
-//     for(let count = 3; count <= steps; count++){
-//         result[count] = result[count-1] + result[count-2] + result[count-3];
-//     }
-//     return result[steps];
-// }
-// console.log('ways to climb: ', __waysToClimb(3));  // input 3, output 4
+// //   // Solution 2  Using dynamic programming     complexity is O(n)
+// //   const __waysToClimb = steps =>{   
+// //     const result = [1,1,2];  
+// //     for(let count = 3; count <= steps; count++){
+// //         result[count] = result[count-1] + result[count-2] + result[count-3];
+// //     }
+// //     return result[steps];
+// // }
+// // console.log('ways to climb: ', __waysToClimb(3));  // input 3, output 4
 
-let out = [];
-let countLengthOfCycle1 = (arr,ind) => {
-  if(!out.includes(arr[ind])){
-    out.push(arr[ind]);
-    countLengthOfCycle1(arr,arr[ind])
-  }else{
-    out.push(arr[ind])
-  }
+// let out = [];
+// let countLengthOfCycle1 = (arr,ind) => {
+//   if(!out.includes(arr[ind])){
+//     out.push(arr[ind]);
+//     countLengthOfCycle1(arr,arr[ind])
+//   }else{
+//     out.push(arr[ind])
+//   }
   
-  return out.lastIndexOf(arr[ind]) - out.indexOf(arr[ind])
+//   return out.lastIndexOf(arr[ind]) - out.indexOf(arr[ind])
+// }
+
+// console.log(countLengthOfCycle1([1,2,3,0,9],4))
+// console.log(out);
+
+// // SI = 0 -- v : 1 --> 1th index 0(val); --> 0th 1
+function countSum(arr,sum){ 
+
+  var arr = [10 ,1, 1,-20 ,10];
+  var sum = 2;
+  var obj={};
+  var count = 0;
+  var cursum= 0;
+
+  for(let i=0;i<arr.length;i++){
+      
+      cursum += arr[i];
+      
+      if(cursum == sum){
+          count++;
+      }
+      
+      if(obj.hasOwnProperty(cursum-sum)){
+          count = count + obj[cursum-sum];
+      }
+
+      obj[cursum] =  obj.hasOwnProperty(cursum) ? obj[cursum]+1:1
+  }
+
+  return count;
 }
 
-console.log(countLengthOfCycle1([1,2,3,0,9],4))
-console.log(out);
 
-// SI = 0 -- v : 1 --> 1th index 0(val); --> 0th 1
+console.log(countSum());
--- src/sample-gs.js
+++ src/sample-gs.js
@@ -26,9 +26,9 @@ function secondSmallest(x)
 function doTestsPass()
 {
   // todo: add more test cases
-  var testArrays  = [[0], [0, 1]];
-  var testResults = [0, 1];
-  
+  var testArrays  = [[0], [-44,-44,-4,-3,22,5,-11,5,89,0,22], [5,22,65,1,666,6] ];
+  var testResults = [0,-11, 5];
+
   // Run through the tests and make assertions
   for(var i = 0; i < testArrays.length; i++)
   {
@@ -39,7 +39,6 @@ function doTestsPass()
   }
   return true;
 }
-
 
 /**
  * Main execution entry.
@@ -52,8 +51,6 @@ else
 {
   console.log("There are test failures.");
 }
-
-
 
 
 /**
@@ -97,3 +94,107 @@ else
 }
 
 
+/* Problem Name is &&& Find longest substring with start index &&& PLEASE DO NOT REMOVE THIS LINE. */
+
+/**
+ * Instructions to candidate.
+ * 1) Run this code in the REPL to observe its behaviour. The
+ *    execution entry point is main().
+ * 2) Consider adding some additional tests in doTestsPass().
+ * 3) Implement secondSmallest() correctly.
+ * 4) If time permits, some possible follow-ups.
+ */
+
+
+/**
+ * Returns the longest substring with start index.
+ * Returns 0 if the string has fewer than 2 letters.
+ */
+function longestSubString(x)
+{
+   return 0;
+}
+
+
+/**
+ * Returns true if all tests pass; otherwise, returns false.
+ */
+function doTestsPass()
+{
+  // todo: add more test cases
+  var testArrays = [['abcabdddd'],['100100000'],['']];
+  var testResults = [[5,4],[4,5],[-1,0]];
+  
+  // Run through the tests and make assertions
+  for(var i = 0; i < testArrays.length; i++){
+    if(longestSubString(testArrays[i]) != testResults[i])
+    {
+      return false;
+    }
+  }  
+    return true;
+  
+}
+
+
+/**
+ * Main execution entry.
+ */
+if(doTestsPass())
+{
+  console.log("All tests pass!");
+}
+else
+{
+  console.log("There are test failures.");
+}
+
+
+
+// Write a JavaScript function that takes a string which has lower and upper case letters as a parameter and converts upper case letters to lower case, and lower case letters to upper case. Go to the editor
+// Test Data :
+// console.log(swapcase('AaBbc'));
+// "aAbBC"
+
+
+// President Electin
+
+var _ = require('underscore');
+
+var solution = (function(){
+
+function whoIsElected(n, k){
+  return -1;
+}
+
+function doTestsPass(testCases){
+  var passes = true;
+  for(var i=0; i<testCases.length; i++){
+    const testCase = testCases[i];
+    const expectedResult = testCase[2];
+    const actualResult = whoIsElected(testCase[0], testCase[1]);
+    passes = passes && (expectedResult === actualResult);
+  }
+  return passes;
+}
+
+return {
+  doTestsPass: doTestsPass,
+  whoIsElected: whoIsElected
+};
+
+})();
+
+const testCases = [
+  [1,1,1],
+  [2,2,1],
+  [4,2,1],
+  [100,2,73]
+];
+
+
+if(solution.doTestsPass(testCases)){
+  console.log('All tests pass');
+}else {
+  console.error("Not all tests pass:(");
+}
